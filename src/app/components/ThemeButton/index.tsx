7880651a986469e0525ff4e85c83029636e673a1
Nick Belvin <nickbelvin@Nicks-MacBook-Air.local>
2024-09-20 08:36:34 -0400

    cleaned up themeing

--- src/app/components/ThemeButton/index.tsx
+++ src/app/components/ThemeButton/index.tsx
@@ -1,47 +1,24 @@
 'use client';
-import { ThemeCookieName } from '@/app/lib/constants';
-import { useIsClient } from '@/app/providers/client-provider';
-import { ActionIcon, Tooltip } from '@mantine/core';
+import {
+  ActionIcon,
+  Tooltip,
+  Transition,
+  useMantineColorScheme,
+} from '@mantine/core';
 import { IconMoon, IconSun } from '@tabler/icons-react';
-import { getCookie, setCookie } from 'cookies-next';
-import { useEffect, useState } from 'react';
 
 export const ThemeButton = () => {
-  const isClient = useIsClient();
-
-  useEffect(() => {
-    const theme = getCookie(ThemeCookieName);
-
-    if (theme === undefined && isClient) {
-      const dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      return setTheme(dark ? 'dark' : 'light');
-    }
-
-    if (theme === undefined) {
-      return setTheme('light');
-    }
-
-    return setTheme(theme);
-  }, [isClient]);
-
-  const [theme, setTheme] = useState('light');
-
-  const handleThemeChange = () => {
-    const newTheme = theme === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    setCookie(ThemeCookieName, newTheme);
-    isClient && window.location.reload();
-  };
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
     <Tooltip label="Toggle Color Scheme">
       <ActionIcon
         size={'lg'}
-        value={theme === 'dark' ? 'dark' : 'light'}
-        onClick={handleThemeChange}
+        value={colorScheme === 'dark' ? 'dark' : 'light'}
+        onClick={toggleColorScheme}
         variant="subtle"
       >
-        {theme !== 'light' ? (
+        {colorScheme !== 'light' ? (
           <IconMoon color="var(--mantine-primary-color-filled)" />
         ) : (
           <IconSun color="var(--mantine-color-yellow-5)" />
